7fa98d0aadf6569b6d15a73204541a5f04997053
Taylor Newill <tanewill@microsoft.com>
2017-06-29 19:41:27 -0700

    updated for latest MPI version

--- full-pingpong.sh
+++ full-pingpong.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Example usage: ./full-pingpong.sh | grep -e ' 512 ' -e NODES -e usec
 
+
+mpiversion=`ls /opt/intel/impi/`
+source /opt/intel/impi/$mpiversion/bin64/mpivars.sh
 for NODE in `cat ~/nodenames.txt`; \
     do for NODE2 in `cat ~/nodenames.txt`; \
         do echo '##################################################' && \
             echo NODES: $NODE, $NODE2 && \
             echo '##################################################' && \
-            /opt/intel/impi/5.1.3.181/bin64/mpirun \
+            /opt/intel/impi/$mpiversion/bin64/mpirun \
             -hosts $NODE,$NODE2 -ppn 1 -n 2 \
-            -env I_MPI_FABRICS=dapl \
+            -env I_MPI_FABRICS=shm:dapl \
             -env I_MPI_DAPL_PROVIDER=ofa-v2-ib0 \
-            -env I_MPI_DYNAMIC_CONNECTION=0 /opt/intel/impi/5.1.3.181/bin64/IMB-MPI1 pingpong; \
+            -env I_MPI_DYNAMIC_CONNECTION=0 /opt/intel/impi/$mpiversion/bin64/IMB-MPI1 pingpong; \
         done; \
     done
-
