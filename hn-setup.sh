5fb02b7d56c8aff68a2158394260398501098d0c
Taylor Newill <tanewill@microsoft.com>
2017-08-23 11:50:12 -0400

    updated sshpass

--- hn-setup.sh
+++ hn-setup.sh
@@ -8,6 +8,7 @@ LICIP=$5
 
 IP=`ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 localip=`echo $IP | cut --delimiter='.' -f -3`
+myhost=`hostname`
 
 echo User is: $USER
 echo Pass is: $PASS
@@ -19,33 +20,29 @@ cat << EOF >> /etc/security/limits.conf
 *               soft    memlock         unlimited
 EOF
 
+#Create directories needed for configuration
 mkdir -p /home/$USER/.ssh
 mkdir -p /home/$USER/bin
-mkdir -p /mnt/resource/scratch
-mkdir -p /mnt/nfsshare
+mkdir -p /mnt/resource/scratch/applications
+mkdir -p /mnt/resource/scratch/INSTALLERS
+mkdir -p /mnt/resource/scratch/benchmark
 
-mkdir /mnt/resource/scratch/
-mkdir /mnt/resource/scratch/applications
-mkdir /mnt/resource/scratch/INSTALLERS
-mkdir /mnt/resource/scratch/benchmark
+#Following lines are only needed if the head node is an RDMA connected VM
+#impi_version=`ls /opt/intel/impi`
+#source /opt/intel/impi/${impi_version}/bin64/mpivars.sh
+#ln -s /opt/intel/impi/${impi_version}/intel64/bin/ /opt/intel/impi/${impi_version}/bin
+#ln -s /opt/intel/impi/${impi_version}/lib64/ /opt/intel/impi/${impi_version}/lib
 
-
-
-impi_version=`ls /opt/intel/impi`
-source /opt/intel/impi/${impi_version}/bin64/mpivars.sh
-
-ln -s /opt/intel/impi/${impi_version}/intel64/bin/ /opt/intel/impi/${impi_version}/bin
-ln -s /opt/intel/impi/${impi_version}/lib64/ /opt/intel/impi/${impi_version}/lib
-
+#Install needed packages
 yum install -y -q epel-release
 yum install -y -q nfs-utils sshpass nmap htop pdsh screen git psmisc
 yum groupinstall -y "X Window System"
 
-myhost=`hostname`
+#Use ganglia install script to install ganglia, this is downloaded via the ARM template
 chmod +x install_ganglia.sh
 ./install_ganglia.sh $myhost azure 8649
 
-echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
+#Setup the NFS server
 echo "/mnt/resource/scratch $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 chmod -R 777 /mnt/nfsshare/
 systemctl enable rpcbind
@@ -61,9 +58,8 @@ systemctl restart nfs-server
 mv clusRun.sh cn-setup.sh /home/$USER/bin
 chmod +x /home/$USER/bin/*.sh
 chown $USER:$USER /home/$USER/bin
-
 nmap -sn $localip.* | grep $localip. | awk '{print $5}' > /home/$USER/bin/hostips
-myhost=`hostname -i`
+
 sed -i '/\<'$myhost'\>/d' /home/$USER/bin/hostips
 sed -i '/\<10.0.0.1\>/d' /home/$USER/bin/hostips
 
@@ -83,12 +79,12 @@ NAMES=`cat /home/$USER/bin/hostips` #names from names.txt file
 
 for name in `cat /home/$USER/bin/hostips`; do\
         sshpass -p "$PASS" ssh $USER@$name "mkdir -p .ssh" && \
-        cat /home/$USER/.ssh/config | sshpass -p "$PASS" ssh $name "cat >> .ssh/config" && \
-        cat /home/$USER/.ssh/id_rsa | sshpass -p "$PASS" ssh $name "cat >> .ssh/id_rsa" && \
-        cat /home/$USER/.ssh/id_rsa.pub | sshpass -p "$PASS" ssh $name "cat >> .ssh/authorized_keys" && \
-        sshpass -p "$PASS" ssh $name "chmod 700 .ssh; chmod 640 .ssh/authorized_keys; chmod 400 .ssh/config; chmod 400 .ssh/id_rsa" && \
-        cat /home/$USER/bin/hostips | ssh $name "cat >> /home/$USER/hostips" && \
-        cat /home/$USER/bin/hosts | ssh $name "cat >> /home/$USER/hosts" ; \
+        cat /home/$USER/.ssh/config | sshpass -p "$PASS" ssh $USER@$name "cat >> .ssh/config" && \
+        cat /home/$USER/.ssh/id_rsa | sshpass -p "$PASS" ssh $USER@$name "cat >> .ssh/id_rsa" && \
+        cat /home/$USER/.ssh/id_rsa.pub | sshpass -p "$PASS" ssh $USER@$name "cat >> .ssh/authorized_keys" && \
+        sshpass -p "$PASS" ssh $USER@$name "chmod 700 .ssh; chmod 640 .ssh/authorized_keys; chmod 400 .ssh/config; chmod 400 .ssh/id_rsa" && \
+        cat /home/$USER/bin/hostips | ssh $USER@$name "cat >> /home/$USER/hostips" && \
+        cat /home/$USER/bin/hosts | ssh $USER@$name "cat >> /home/$USER/hosts" ; \
 done
 
 cp ~/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys
