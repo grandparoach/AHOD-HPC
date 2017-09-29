3afdc82116b9160d937d34f2ff16e1295a098b7e
Taylor Newill <tanewill@microsoft.com>
2017-09-29 15:36:01 -0400

    OF updates

--- solvers/install-OpenFOAM.sh
+++ solvers/install-OpenFOAM.sh
@@ -6,14 +6,15 @@ LICIP=$2
 HOST=`hostname`
 DOWN=$3
 echo $USER,$LICIP,$HOST,$DOWN
+touch HELLO
+export SHARE_DATA=$SHARE_DATA
+export SHARE_HOME=/home/$USER
 
-wget -q http://azbenchmarkstorage.blob.core.windows.net/foambenchmarkstorage/20170524_PE_OpenFOAM.tgz -O /mnt/resource/scratch/INSTALLERS/OF_IMPI.tgz
-tar -xzf /mnt/resource/scratch/INSTALLERS/OF_IMPI.tgz -C /mnt/resource/scratch/applications/
-rm /mnt/resource/scratch/INSTALLERS/*.tgz
+wget -q http://azbenchmarkstorage.blob.core.windows.net/foambenchmarkstorage/20170524_PE_OpenFOAM.tgz -O $SHARE_DATA/INSTALLERS/OF_IMPI.tgz
+tar -xzf $SHARE_DATA/INSTALLERS/OF_IMPI.tgz -C $SHARE_DATA/applications/
+rm $SHARE_DATA/INSTALLERS/*.tgz
 
-echo source /mnt/resource/scratch/applications/OpenFOAM/OpenFOAM-4.x/etc/bashrc >>  /home/$USER/.bashrc
+echo source $SHARE_DATA/applications/OpenFOAM/OpenFOAM-4.x/etc/bashrc >>  /home/$USER/.bashrc
 
-
-
-chown -R $USER:$USER /mnt/resource/scratch/*
+chown -R $USER:$USER $SHARE_DATA/*
 chown -R $USER:$USER /mnt/nfsshare
