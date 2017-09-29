b9fecad98acc2ecf53533a0ef4932c4199a15f10
Taylor Newill <tanewill@microsoft.com>
2017-09-29 16:09:09 -0400

    remove the rm .tgz line

--- solvers/install-OpenFOAM.sh
+++ solvers/install-OpenFOAM.sh
@@ -12,7 +12,7 @@ export SHARE_HOME=/home/$USER
 
 wget -q http://azbenchmarkstorage.blob.core.windows.net/foambenchmarkstorage/20170524_PE_OpenFOAM.tgz -O $SHARE_DATA/INSTALLERS/OF_IMPI.tgz
 tar -xzf $SHARE_DATA/INSTALLERS/OF_IMPI.tgz -C $SHARE_DATA/applications/
-rm $SHARE_DATA/INSTALLERS/*.tgz
+#rm $SHARE_DATA/INSTALLERS/*.tgz
 
 echo source $SHARE_DATA/applications/OpenFOAM/OpenFOAM-4.x/etc/bashrc >>  /home/$USER/.bashrc
 
