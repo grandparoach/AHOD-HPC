c3327c877c419417f5ae3f3780f5fcd51ae60ca2
Taylor Newill <tanewill@microsoft.com>
2017-10-19 18:33:11 -0700

    fixing wget issues

--- solvers/install-OpenFOAM.sh
+++ solvers/install-OpenFOAM.sh
@@ -10,8 +10,8 @@ echo $USER,$LICIP,$HOST,$DOWN
 export SHARE_DATA=/mnt/resource/scratch
 export SHARE_HOME=/home/$USER
 
-wget -q http://azbenchmarkstorage.blob.core.windows.net/foambenchmarkstorage/20170524_PE_OpenFOAM.tgz -O $SHARE_DATA/INSTALLERS/OF_IMPI.tgz
-tar -xzf $SHARE_DATA/INSTALLERS/OF_IMPI.tgz -C $SHARE_DATA/applications/
+wget -q http://azbenchmarkstorage.blob.core.windows.net/foambenchmarkstorage/20170524_PE_OpenFOAM.tgz -O ~/OF_IMPI.tgz
+tar -xzf ~/OF_IMPI.tgz -C $SHARE_DATA/applications/
 #rm $SHARE_DATA/INSTALLERS/*.tgz
 
 echo source $SHARE_DATA/applications/OpenFOAM/OpenFOAM-4.x/etc/bashrc >>  /home/$USER/.bashrc
